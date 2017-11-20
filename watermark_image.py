6e97863ab86d2dd8154dd31175ecd46cfc8f1bb3
pethomas <pethomas@cisco.com>
2017-11-20 10:41:23 +1000

    more

--- watermark_image.py
+++ watermark_image.py
@@ -11,6 +11,10 @@ import matplotlib.cbook as cbook
 import matplotlib.image as image
 import matplotlib.pyplot as plt
 
+from io import BytesIO
+
+buff = BytesIO()
+
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
@@ -20,12 +24,15 @@ image_name = 'FloorPlanCopy.png'
 datafile = cbook.get_sample_data('logo2.png', asfileobj=False)
 print('loading %s' % datafile)
 im = image.imread(image_path + image_name)
-im[:, :, -1] = 0.5  # set the alpha channel
+im[1, 1, 1] = 0 # set the alpha channel
 
 fig, ax = plt.subplots()
-
-ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=0.7, mfc='green')
+plt.scatter(0.5,0.5, facecolor='r', edgecolor='r')
+ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=1, mfc='green')
 ax.grid()
-fig.figimage(im, 10, 10, zorder=3)
+# negative z order puts image behind the graph
+fig.figimage(im, 10, 10, zorder=-11)
 
+plt.axis('off')
+plt.savefig(buff, format='png', dpi=500)
 plt.show()
